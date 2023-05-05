81e617e8ed6272ecbc7021d89e4c21dbddcaa3ae
skizzle010 <raiutkarsh3219@gmail.com>
2023-05-05 05:41:14 +0530

    sharebutton-done

--- components/ShareButton.js
+++ components/ShareButton.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const ShareButton = () => {
     const  router = useRouter();
     const copyLink = () => {
-
+        navigator.clipboard.writeText(`https://localhost:3000/${router.query.handle}`)
+        toast.success('Link copied to clipboard')
 
     }
     return(
     <>
-       <div className="flex flex-row justify-center my-9">
+       <div className="flex flex-row justify-center my-9" onClick={copyLink}>
        <img classname="w-10" src="/svg/send.svg" alt="share"/>
        <p className="ml-3">Click to share</p>
        </div>
